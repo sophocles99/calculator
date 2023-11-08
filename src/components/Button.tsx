804fa9101afb9fd0e0c6d2a70ae14b61d8ab9bef
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-08 11:48:36 +0000

    rename props in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,20 @@
 import { ButtonDef } from "../App";
 import styles from "../styles/Button.module.css";
 
-export default function Button ({ content, style: type, onClick, doubleWidth: isDoubleWidth }: ButtonDef) {
+export default function Button({
+  content,
+  style,
+  onClick,
+  doubleWidth,
+}: ButtonDef) {
   return (
     <button
-      className={`${styles.button} ${styles[type]} ${
-        isDoubleWidth ? styles.doubleWidth : ""
+      className={`${styles.button} ${styles[style]} ${
+        doubleWidth ? styles.doubleWidth : ""
       }`}
       onClick={onClick}
     >
       {content}
     </button>
   );
-};
+}
