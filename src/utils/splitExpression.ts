386f989dc6b9a9e12913381f6e005c23c281285f
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-13 16:27:19 +0000

    split expression display lines at commas

--- src/utils/splitExpression.ts
+++ src/utils/splitExpression.ts
@@ -16,12 +16,18 @@ export default function splitExpression(
   for (let i = expression.length - 1; i >= 0; i--) {
     currentLine = expression[i] + currentLine;
     DOMtestLine.textContent = currentLine;
-
+    
     const testLineWidth = DOMtestLine.getBoundingClientRect().width;
     if (testLineWidth > containerWidth) {
-      expressionLines.unshift(currentLine.slice(1));
-      currentLine = currentLine[0];
-      DOMtestLine.textContent = "";
+      const commaIndex = currentLine.indexOf(",");
+      if (commaIndex >= 0 && commaIndex <= 3) {
+        console.log(currentLine, currentLine.slice(3), currentLine.slice(0, commaIndex))
+        expressionLines.unshift(currentLine.slice(3));
+        currentLine = currentLine.slice(0, commaIndex + 1);
+      } else {
+        expressionLines.unshift(currentLine.slice(1));
+        currentLine = currentLine[0];
+      }
     }
   }
   if (currentLine) {
