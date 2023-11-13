76297cbae30d2296d559e9c958c4efa7c9d67f7d
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-13 16:52:08 +0000

    split expression display lines at operators

--- src/utils/splitExpression.ts
+++ src/utils/splitExpression.ts
@@ -1,3 +1,4 @@
+import { OPERATORS_REGEX } from "./calculatorLogicReducer";
 import styles from "../styles/Expression.module.css";
 
 export default function splitExpression(
@@ -6,21 +7,25 @@ export default function splitExpression(
 ) {
   const containerWidth = container.getBoundingClientRect().width;
 
-  const DOMtestLine = document.createElement("p");
-  DOMtestLine.classList.add(styles.testLine);
-  document.body.appendChild(DOMtestLine);
+  const DOMTestLine = document.createElement("p");
+  DOMTestLine.classList.add(styles.testLine);
+  document.body.appendChild(DOMTestLine);
 
   const expressionLines = [];
   let currentLine = "";
 
   for (let i = expression.length - 1; i >= 0; i--) {
     currentLine = expression[i] + currentLine;
-    DOMtestLine.textContent = currentLine;
-    
-    const testLineWidth = DOMtestLine.getBoundingClientRect().width;
+    DOMTestLine.textContent = currentLine;
+
+    const testLineWidth = DOMTestLine.getBoundingClientRect().width;
     if (testLineWidth > containerWidth) {
+      const operatorIndex = currentLine.search(OPERATORS_REGEX);
       const commaIndex = currentLine.indexOf(",");
-      if (commaIndex >= 0 && commaIndex <= 3) {
+      if (operatorIndex >= 0 && operatorIndex <= 3) {
+        expressionLines.unshift(currentLine.slice(operatorIndex));
+        currentLine = currentLine.slice(0, operatorIndex);
+      } else if (commaIndex >= 0 && commaIndex <= 3) {
         expressionLines.unshift(currentLine.slice(commaIndex + 1));
         currentLine = currentLine.slice(0, commaIndex + 1);
       } else {
@@ -32,6 +37,6 @@ export default function splitExpression(
   if (currentLine) {
     expressionLines.unshift(currentLine);
   }
-  document.body.removeChild(DOMtestLine);
+  document.body.removeChild(DOMTestLine);
   return expressionLines;
 }
