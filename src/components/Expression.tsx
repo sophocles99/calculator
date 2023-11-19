cf351ac187aef4a777d6b23a51160f87de42de81
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-19 15:10:06 +0000

    tweaks

--- src/components/Expression.tsx
+++ src/components/Expression.tsx
@@ -5,14 +5,17 @@ import fitExpression from "../utils/fitExpression";
 import styles from "../styles/Expression.module.css";
 
 export default function Expression() {
-  const { state, dispatch } = useContext(CalculatorLogicContext);
+  const {
+    state: { expression },
+    dispatch,
+  } = useContext(CalculatorLogicContext);
   const [expressionFormattedLines, setExpressionFormattedLines] = useState<
     string[]
   >([]);
   const containerRef = useRef<HTMLDivElement>(null);
   const container = containerRef.current;
-  
-  const {expression} = state;
+
+  // const {expression} = state;
   const expressionFormatted = formatExpression(expression);
 
   useEffect(() => {
