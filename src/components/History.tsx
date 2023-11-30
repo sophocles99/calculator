b5697b573d1143bfc36e6d31f773d81f071690f7
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-11-30 13:00:28 +0000

    create ref for History container and array of refs for historyLines and use to set scrollbar positions

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,17 +1,47 @@
+import { RefObject, useEffect, useRef, useState } from "react";
 import addCommaSeparators from "../utils/addCommaSeparators";
 import formatExpression from "../utils/formatExpression";
 import styles from "../styles/History.module.css";
 
 export default function History() {
-  let storedHistory: string[] = [];
-  const storedHistoryJSON = localStorage.getItem("history");
-  if (storedHistoryJSON) {
-    storedHistory = JSON.parse(storedHistoryJSON);
-  }
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [historyLines, setHistoryLines] = useState<PreviousExpressionType[]>(
+    []
+  );
+  const historyLinesRef = useRef<(HTMLParagraphElement | null)[]>([]);
+
+  useEffect(() => {
+    let storedHistoryLines: PreviousExpressionType[] = [];
+    const storedHistoryJSON = localStorage.getItem("history");
+    if (storedHistoryJSON) {
+      storedHistoryLines = JSON.parse(storedHistoryJSON);
+    }
+    setHistoryLines(storedHistoryLines);
+  }, []);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container && container.scrollHeight > container.clientHeight) {
+      container.scrollTop = 9999;
+    }
+    const historyParagraphs = historyLinesRef.current;
+    if (historyParagraphs) {
+      historyParagraphs.forEach((paragraph) => {
+        if (paragraph && paragraph.scrollWidth > paragraph.clientWidth) {
+          paragraph.scrollLeft = 9999;
+        }
+      });
+    }
+  }, [historyLines]);
+
   return (
-    <div className={styles.historyContainer}>
-      {storedHistory.map(([expression, answer], index) => (
-        <p className={styles.historyLine} key={index}>
+    <div ref={containerRef} className={styles.historyContainer}>
+      {historyLines.map(([expression, answer], index) => (
+        <p
+          ref={(element) => (historyLinesRef.current[index] = element)}
+          className={styles.historyLine}
+          key={index}
+        >
           {formatExpression(expression)} = {addCommaSeparators(answer)}
         </p>
       ))}
