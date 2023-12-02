986e9a008e28fd8cfaeb41a0f1a369ce5730a281
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-02 15:02:41 +0000

    change title to History when history is open

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
+import { HistoryContext } from "../contexts/History";
 import Nav from "./Nav";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
+  const { historyState } = useContext(HistoryContext);
+  const { historyIsOpen } = historyState;
+  const title = historyIsOpen ? "History" : "Calculator";
+
   return (
     <header className={styles.header}>
       <Nav />
-      <p className={styles.title}>Calculator</p>
+      <p className={styles.title}>{title}</p>
     </header>
   );
 }
